ef6053c824ca41889254256b55db910819a4d680
FUTGEND <kellyriglet@gmail.com>
2024-05-13 00:39:58 +0200

    Update accueil.js

--- js/accueil.js
+++ js/accueil.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 <div class="sticky-note" style="background-color: ${post.code_couleur_post};">
                     <div class="note-title"><a class="title-link" href="../afficher_post.php?id=${post.id_post}" style="text-decoration:none">${post.titre_post}</a></div>
                     <div class="note-date">${post.date_creation_post}</div>
-                    <button href="#"> Modifier </button>
-                    <button href="#"> Supprimer </button>
+                    <a class="button" href="../modifier_post.php?id=${post.id_post}" style="text-decoration:none;">Modifier</a>
+                    <a class="button" href="../supprimer_post.php?id=${post.id_post}" style="text-decoration:none;">Supprimer</a>
 
                 </div>
             `;
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function() {
         data.sharedPosts.forEach(post => {
             sharedContainer.innerHTML += `
                 <div class="sticky-note" style="background-color: ${post.code_couleur_post};">
-                    <div class="note-title"><a class="title-link" href="afficher_post.php?id=${post.id_post}" style="text-decoration:none">${post.titre_post}</a></div>
+                    <div class="note-title"><a class="title-link" href="../afficher_post.php?id=${post.id_post}" style="text-decoration:none">${post.titre_post}</a></div>
                     <div class="note-date">${post.date_creation_post}</div>
                 </div>
             `;
