66d49e82a684e6511598314d7f42c979da77d5fe
FUTGEND <80228596+Futgend@users.noreply.github.com>
2024-05-12 17:54:17 +0200

    Update accueil.js

--- js/accueil.js
+++ js/accueil.js
@@ -8,9 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
             postContainer.innerHTML += `
                 <div class="sticky-note" style="background-color: ${post.code_couleur_post}; font-family: ${post.police_post}; font-size: ${post.taille_post};">
                     <div class="note-title"><a class="title-link" href="#" style="text-decoration:none">${post.titre_post}</a></div>
-                    <div class="note-content">${post.contenu_post}</div>
-                    <div class="note-date">${post.date_creation_post}</div>
-                    <div class="note-modified">${post.date_derniere_modif_post}</div>
+                    <div class="note-date">${post.date_creation_post}</div>                    
                 </div>
             `;
         });
