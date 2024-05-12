b236262e004b7f1e922ee49fc52c1a59490ec49f
FUTGEND <80228596+Futgend@users.noreply.github.com>
2024-05-12 19:20:38 +0200

    Update accueil.js

--- js/accueil.js
+++ js/accueil.js
@@ -1,14 +1,29 @@
 document.addEventListener('DOMContentLoaded', function() {
     fetch('php/accueil.php')
     .then(response => response.json())
-    .then(posts => {
-        const postContainer = document.getElementById('ownedPostIts'); // Utilisez 'ownedPostIts' ou un autre conteneur approprié
+    .then(data => {
+        const ownedContainer = document.getElementById('ownedPostIts');
+        const sharedContainer = document.getElementById('sharedPostIts');
 
-        posts.forEach(post => {
-            postContainer.innerHTML += `
+        // Affichage des post-its possédés
+        data.ownedPosts.forEach(post => {
+            ownedContainer.innerHTML += `
+                <div class="sticky-note" style="background-color: ${post.code_couleur_post}; font-family: ${post.police_post}; font-size: ${post.taille_post};">
+                    <div class="note-title"><a class="title-link" href="../afficher_post_2.php" style="text-decoration:none">${post.titre_post}</a></div>
+                    <div class="note-date">${post.date_creation_post}</div>
+                    <button href="#"> Modifier </button>
+                    <button href="#"> Supprimer </button>
+
+                </div>
+            `;
+        });
+
+        // Affichage des post-its partagés
+        data.sharedPosts.forEach(post => {
+            sharedContainer.innerHTML += `
                 <div class="sticky-note" style="background-color: ${post.code_couleur_post}; font-family: ${post.police_post}; font-size: ${post.taille_post};">
                     <div class="note-title"><a class="title-link" href="#" style="text-decoration:none">${post.titre_post}</a></div>
-                    <div class="note-date">${post.date_creation_post}</div>                    
+                    <div class="note-date">${post.date_creation_post}</div>
                 </div>
             `;
         });
