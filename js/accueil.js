18ed9e9f9d6cb3ca374dac0893d9bc3a52cdee43
FUTGEND <kellyriglet@gmail.com>
2024-05-13 20:03:29 +0200

    Update accueil.css et accueil.js + ajout icon

--- js/accueil.js
+++ js/accueil.js
@@ -4,15 +4,29 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(data => {
         const ownedContainer = document.getElementById('ownedPostIts');
         const sharedContainer = document.getElementById('sharedPostIts');
+        const dateFormatter = new Intl.DateTimeFormat('fr-FR', {
+            year: '2-digit',
+            month: '2-digit',
+            day: '2-digit',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: false // Utilisation du format 24 heures
+        });
+
+        // Fonction pour formater la date
+        function formatPostItDate(dateString) {
+            const date = new Date(dateString);
+            return dateFormatter.format(date);
+        }
 
         // Affichage des post-its possédés
         data.ownedPosts.forEach(post => {
             ownedContainer.innerHTML += `
                 <div class="sticky-note" style="background-color: ${post.code_couleur_post};">
                     <div class="note-title"><a class="title-link" href="../afficher_post.php?id=${post.id_post}" style="text-decoration:none">${post.titre_post}</a></div>
-                    <div class="note-date">${post.date_creation_post}</div>
-                    <a class="button" href="../php/modifier_post.php?id=${post.id_post}" style="text-decoration:none;">Modifier</a>
-                    <a class="button" href="../php/supprimer_post.php?id=${post.id_post}" style="text-decoration:none;">Supprimer</a>
+                    <div class="note-date">${formatPostItDate(post.date_creation_post)}</div>
+                    <div class="note-button"><a class="button" href="../php/modifier_post.php?id=${post.id_post}" style="text-decoration:none;"><img src="../icons/pencil-square.svg"</a>
+                    <a class="button" href="../php/supprimer_post.php?id=${post.id_post}" style="text-decoration:none;"><img src="../icons/trash-fill.svg"</a></div>
 
                 </div>
             `;
@@ -23,8 +37,12 @@ document.addEventListener('DOMContentLoaded', function() {
             sharedContainer.innerHTML += `
                 <div class="sticky-note" style="background-color: ${post.code_couleur_post};">
                     <div class="note-title"><a class="title-link" href="../afficher_post.php?id=${post.id_post}" style="text-decoration:none">${post.titre_post}</a></div>
-                    <div class="note-date">${post.date_creation_post}</div>
-                    <div class="note-user"><p>crée par ${post.username_utilisateur}</p></div>
+                    <div class="note-date">${formatPostItDate(post.date_creation_post)}</div>
+                    <div class="note-user">
+                        <div>
+                            <em>crée par ${post.username_utilisateur}</em>
+                        </div>
+                    </div>
                 </div>
             `;
         });
