6d07a2634bdaaf2aa6e6bb4fe3287bad4ccd3af0
valentine315 <107465774+valentine315@users.noreply.github.com>
2024-05-14 17:03:07 +0200

    Update supprimer_post.php

--- php/supprimer_post.php
+++ php/supprimer_post.php
@@ -1,5 +1,10 @@
 <?php
     session_start();
+    if (!isset($_SESSION['id'])) {
+        header('Location: connexion.php');
+        exit();
+    }
+    $user_id = $_SESSION['id'];
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -23,16 +28,19 @@
         if (isset($_GET['id'])) {
             $post_id = $_GET['id'];
             include '../database/db_co.php';
-            $sql = "SELECT titre_post FROM post WHERE id_post = :post_id";
+          
+            $sql = "SELECT titre_post FROM post WHERE id_post = :post_id AND id_utilisateur = :user_id";
             $stmt = $db->prepare($sql);
             $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
             $stmt->execute();
+
             if ($stmt->rowCount() > 0) {
                 $post_title = $stmt->fetch(PDO::FETCH_ASSOC)['titre_post'];
                 $escaped_post_title = htmlspecialchars($post_title, ENT_QUOTES, 'UTF-8');
                 echo "<p>Voulez-vous vraiment supprimer votre post-it intitulé : \"$escaped_post_title\" ?</p>";
             } else {
-                echo "<p>Aucun post trouvé avec cet ID.</p>";
+                echo "<p>Aucun post trouvé avec cet ID ou vous n'avez pas la permission de le supprimer.</p>";
             }
         } else {
             echo "<p>Aucun ID de post spécifié.</p>";
@@ -40,7 +48,8 @@
         ?>
 
         <form action="supp_post_2.php" method="post">
-            <a class="delete-button" href='supp_post_2.php?id=<?php echo htmlspecialchars($post_id, ENT_QUOTES, 'UTF-8'); ?>' style="text-decoration: none;">Supprimer</a>
+            <input type="hidden" name="id" value="<?php echo htmlspecialchars($post_id, ENT_QUOTES, 'UTF-8'); ?>">
+            <button type="submit" class="delete-button">Supprimer</button>
             <button type="button" class="cancel-button" onclick="window.location.href = 'accueil.php';">Annuler</button>
         </form>
     </div>
