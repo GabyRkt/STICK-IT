9f8571ee738bd5f224cfb622643db5661300a8ab
Jade V <79988208+JadeVinsonPro@users.noreply.github.com>
2024-05-14 23:47:25 +0200

    Update details_post.php

--- php/details_post.php
+++ php/details_post.php
@@ -11,18 +11,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Détails Post IT - Stick IT</title>
+    <title>Détails post</title>
     <link rel="stylesheet" type="text/css" href="../CSS/details_post.css">
     <script src="scripts.js"></script>
 </head>
 
 <body>
-<header>
-    <?php
-    require_once('../php/nav.php');
-    ?>
+    <header>
+        <?php
+        require_once('../php/nav.php');
+        ?>
+        </header>
+        <?php include '../database/db_co.php'; ?>
     </header>
-    <?php include '../database/db_co.php'; ?>
 
     <?php
     // Vérifie si l'identifiant du post est passé en paramètre dans l'URL
@@ -59,9 +60,12 @@
             $date_creation_formatee = strftime("%A %e %B %Y à %H:%M", $date_creation_obj->getTimestamp());
             $date_modification_formatee = strftime("%A %e %B %Y à %H:%M", $date_modification_obj->getTimestamp());
 
-            // Affichage de la date formatée
-            echo $date_creation_formatee; 
-            echo $date_modification_formatee; 
+
+            $sql = "SELECT id_post, titre_post, contenu_post, code_couleur_post, police_post, taille_post FROM post WHERE id_post = :post_id AND id_utilisateur = :user_id";
+            $stmt = $db->prepare($sql);
+            $stmt->bindParam(':post_id', $post_id, PDO::PARAM_INT);
+            $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+            $stmt->execute();
 
             // Récupérer le username du propriétaire 
             $sql_utilisateur = "SELECT utilisateur.username_utilisateur FROM post 
@@ -80,52 +84,52 @@
             $stmt_partage->bindParam(':post_id', $post_id, PDO::PARAM_INT);
             $stmt_partage->execute();
             $emails_partages = $stmt_partage->fetchAll(PDO::FETCH_COLUMN);
-
-?>
-           <div class='titre'><?php echo $_SESSION['username']; ?></div>
+            ?>
+            
+           <div class='titre'></div>
            <div class='container-post' style='text-align: center;background-color:<?php echo $row['code_couleur_post']; ?>;'>
-                    <div class='titre' style="display: flex; align-items: center; justify-content: center;"> 
-                        <?php echo $row['titre_post']; ?>
-                        <div style="margin-left: auto;">
-                            <!-- Affichage des boutons de modification et de suppression -->
-                            <a class="button" href="../php/modifier_post.php?id=<?php echo $_GET['id']; ?>" style="text-decoration:none;"><img src="../icons/pencil-square.svg"></a>
-                            <a class="button" href="../php/supprimer_post.php?id=<?php echo $_GET['id']; ?>" style="text-decoration:none;"><img src="../icons/trash-fill.svg"></a>
-                        </div>
+                <div class='titre' style="display: flex; align-items: center; justify-content: center;"> 
+                    <?php echo htmlspecialchars($row['titre_post'], ENT_QUOTES, 'UTF-8')?>
+                    <div style="margin-left: auto;">
+                    <?php 
+                    // Modification possible que pour le propriétaire du post
+                    if (in_array($_SESSION['username'], $username)) {
+                    ?>
+                        <!-- Affichage des boutons de modification et de suppression -->
+                        <a class="button" href="../php/modifier_post.php?id=<?php echo $_GET['id']; ?>" style="text-decoration:none;"><img src="../icons/pencil-square.svg"></a>
+                        <a class="button" href="../php/supprimer_post.php?id=<?php echo $_GET['id']; ?>" style="text-decoration:none;"><img src="../icons/trash-fill.svg"></a>
+                        <?php
+                            }
+                        ?>
                     </div>
-                    <div class="container">
-                        <p>Propriétaire : <?php echo implode(',', $username);  ?></p>
-                    </div>
-                    <br>
-                    <hr>
-                    <div class='post-text' style='background-color:<?php echo $row['code_couleur_post']; ?>;font-size:<?php echo $row['taille_post']; ?>; font-family:<?php echo $row['police_post']; ?>;'><p><?php echo $row['contenu_post']; ?></p></div>
-                    <hr>
-                    <br>
-                    <br>
-                    <div class="container">
-                        <div>Crée le : <?php echo $date_creation_formatee; ?></div>
-                        <div>Dernière modification le : <?php echo $date_modification_formatee; ?></div>
-                    </div>           
-                    <div class="container">
-                        <h3>Partagé avec :</h3>
-                    </div>
-                    <div class="container">
-                        <?php echo implode(',', $emails_partages); ?>
-                    </div>
-                        <a class='back-link' style='text-align: left; background-color:#FFBC70' href='../php/accueil.php'>Retour à la liste des posts</a> 
-        </div>      
+                </div>
+                <div class="container">
+                    <p>Propriétaire : <?php echo implode(',', $username);  ?></p>
+                </div>
+                <br>
+                <hr>
+                <div class='post-text' style='background-color:<?php echo $row['code_couleur_post']; ?>;font-size:<?php echo $row['taille_post']; ?>; font-family:<?php echo $row['police_post']; ?>;'><p><?php echo htmlspecialchars($row['contenu_post'], ENT_QUOTES, 'UTF-8') ?></p></div>
+                <hr>
+                <br>
+                <br>
+                <div class="container">
+                    <div>Crée le : <?php echo $date_creation_formatee; ?></div>
+                    <div>Dernière modification le : <?php echo $date_modification_formatee; ?></div>
+                </div>
+                <br>
+                <hr>           
+                <div class="container">
+                    <h3>Partagé avec :</h3>
+                </div>
+                <div class="container">
+                    <?php echo implode(',', $emails_partages); ?>
+                </div>
+                    <a class='back-link' style='text-align: left; background-color:#FFBC70' href='../php/accueil.php'>Retour à la liste des posts</a> 
+            </div>      
         <?php
-}
+        }
     } else {
-        ?>
-            <div class='titre'>Identifiant non spécifié</div>
-                <div class='container-post' style='text-align: center;'>
-                    <div class='post-details' style='text-align: start;'> </div>               
-                    <div class='post-text'><p>Veuillez entrer un identifiant</p></div>
-                    <div class='post-details'></div>
-                    <a class='back-link' style='text-align: left;' href='../php/accueil.php'>Retour à la liste des posts</a>   
-            </div>
-
-        <?php
+        echo "<div class='container'>Aucun post trouvé avec cet ID.</div>";
     }
     ?>
 </body>
